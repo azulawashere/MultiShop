977830033bd50feaab4de016c0d77582f80682f4
azulawashere <janana856@hotmail.com>
2024-06-01 15:59:39 +0300

    Product İçin dto classları dolduruldu

---
+++ Services/Catalog/MultiShop.Catalog/Dtos/ProductDtos/GetByIdProductDto.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MultiShop.Catalog.Entities;
+
+namespace MultiShop.Catalog.Dtos.ProductDtos
+{
+    public class GetByIdProductDto
+    {
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal ProductPrice { get; set; }
+        public int ProductStock { get; set; }
+        public string ProduckImageUrl { get; set; }
+        public string ProduckDescription { get; set; }
+        public string CategoryId { get; set; }
+        [BsonIgnore]
+        public Category Category { get; set; }
+    }
+}
---
+++ Services/Catalog/MultiShop.Catalog/Dtos/ProductDtos/ResultProductDto.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MultiShop.Catalog.Entities;
+
+namespace MultiShop.Catalog.Dtos.ProductDtos
+{
+    public class ResultProductDto
+    {
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal ProductPrice { get; set; }
+        public int ProductStock { get; set; }
+        public string ProduckImageUrl { get; set; }
+        public string ProduckDescription { get; set; }
+        public string CategoryId { get; set; }
+       
+    }
+}
