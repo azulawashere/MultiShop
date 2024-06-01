06aa0b01de943370bce36fd3c474d7afea7a01a2
azulawashere <janana856@hotmail.com>
2024-06-01 15:53:20 +0300

    Dtos dosyaları eklendi

---
+++ Services/Catalog/MultiShop.Catalog/Settings/IDatabaseSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace MultiShop.Catalog.Settings
+{
+    public interface IDatabaseSettings
+    {
+        public string CategoryCollection { get; set; }
+        public string ProductCollectionName { get; set; }
+        public string ProductDetailCollectionName { get; set; }
+        public string ProductImageCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+
+    }
+}
