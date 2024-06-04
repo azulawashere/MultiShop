5f4e0b140da6646b0a6fc67357fb422a3f9959e7
azulawashere <janana856@hotmail.com>
2024-06-04 19:43:47 +0300

    paketler yüklendi

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/OrderDetailHandlers/GetOrderDetailByIdQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Queries.OrderDetailsQueries;
+using MultiShop.Order.Application.Features.CQRS.Results.OrderDetailsResults;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entitites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers
+{
+    public class GetOrderDetailByIdQueryHandler
+    {
+        private readonly IRepository<OrderDetail> _repository;
+
+        public GetOrderDetailByIdQueryHandler(IRepository<OrderDetail> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<GetOrderDetailByIdQueryResult> Handle(GetOrderDetailByIdQuery query)
+        {
+           var values = await _repository.GetByIdAsync(query.Id);
+            return new GetOrderDetailByIdQueryResult
+            {
+               ProductAmount=values. ProductAmount,
+               ProductName=values. ProductName,
+               ProductTotalPrice=values. ProductTotalPrice,
+               ProductPrice=values. ProductPrice,   
+               ProductId = values.ProductId ,
+               OrderDetailId = values.OrderDetailId ,
+               OrderingId = values.OrderingId ,
+            };
+        }
+    }
+}
